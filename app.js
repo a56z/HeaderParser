d6a9d7da7a2c0fca48af13fd7856148e2688ce44
Enrique Folte <hhfolte@gmail.com>
2019-03-08 23:48:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//Basic required imports for NodeJS
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+//Used for easy parsing of user-agent for response
+var useragent = require('express-useragent');
+//Create an instance of express for our app and instantiate bodyParser and cors
+var app = module.exports = express('express-useragent');
+app.use(bodyParser.json());
+app.use(cors());
+app.use(useragent.express());
+//API url
+var api = '/api/whoami';
+
+app.get(api, function(req, res, next){
+    var language = req.acceptsLanguages();
+    var software = 'OS: ' + req.useragent.os + ', Browser: ' + req.useragent.browser;
+//req.headers['user-agent']; Same way of getting data for software
+    var ipaddress = req.ip;
+
+    res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
+});
+
+app.listen(3000,function(){
+    console.log("Working");
+});
